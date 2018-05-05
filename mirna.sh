d1932698a0207c748dcbe7c5084c9404f35b29fc
Priit Paluoja <priitpaluoja@gmail.com>
2018-05-05 12:58:11 +0300

    Create mirna.sh

---
+++ mirna.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+#SBATCH --time=60:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=priitpaluoja@gmail.com
+#SBATCH -J sRNAbench
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=15000
+
+# Use in ROCKET only
+module load R-3.3.0
+module load java-1.8.0_40
+module load viennarna-2.8.1
+module load python-2.7.6
+module load fastx_toolkit-0.0.14
+
+home=/gpfs/hpchome/ppaluoja/software/sRNAtoolboxDB
+export PATH="${PATH}:$home/exec/bowtie-1.2.2-linux-x86_64"
+
+for f in *.collapsed.fasta
+do
+	name=$(basename "$f" | cut -d. -f1)
+	echo "$fbname"
+	# FASTQ -> FASTA
+	# Remove adapter + everything following
+	# discard sequences shorter than 18 nucleotides
+	# keep only sequences which contained the adapter
+	# discard sequences longer than 25 nucleotides
+	zcat $f | fastq_to_fasta | fastx_clipper -a "AGATCGGAAG" -l 18 -c | fastx_trimmer -l 25 | fastx_collapser -o $name.fasta
+	# sRNAbench
+	java -Xms20m -Xmx14900m -jar $home/exec/sRNAbench.jar input=$name.fasta output=${name}.out dbPath=$home graphics=true sep="-" microRNA=hsa species=hg38 mature=mature.fa hairpin=hairpin.fa
+done
