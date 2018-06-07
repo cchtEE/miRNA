d8a462e11c14fbca71fbe6ccda9c158e847378b1
Priit Paluoja <priitpaluoja@gmail.com>
2018-06-07 16:34:23 +0300

    Update mirna.sh

--- mirna.sh
+++ mirna.sh
@@ -1,32 +1,33 @@
 #!/bin/sh
-#SBATCH --time=60:00:00
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=priitpaluoja@gmail.com
-#SBATCH -J sRNAbench
-#SBATCH -N 1
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=15000
 
-# Use in ROCKET only
+# Use in ROCKET only, not working in ATLAS
 module load R-3.3.0
 module load java-1.8.0_40
 module load viennarna-2.8.1
 module load python-2.7.6
 module load fastx_toolkit-0.0.14
 
+# Provide locations file with command line argument.
 home=/gpfs/hpchome/ppaluoja/software/sRNAtoolboxDB
 export PATH="${PATH}:$home/exec/bowtie-1.2.2-linux-x86_64"
+locations="locations"
 
-for f in *.gz
-do
-	name=$(basename "$f" | cut -d. -f1)
-	# FASTQ -> FASTA
+while IFS='' read -r line || [[ -n "$line" ]]; do
+	location=$line
+	sample=$(basename "$line" | cut -d. -f1)
+	echo $sample
+	# FASTQ -> FASTAmi
 	# Remove adapter + everything following.
 	# Discard sequences shorter than 18 nucleotides.
 	# Keep only sequences which contained the adapter.
 	# Discard sequences longer than 25 nucleotides.
-	zcat $f | fastq_to_fasta | fastx_clipper -a "AGATCGGAAG" -l 18 -c | fastx_trimmer -l 25 | fastx_collapser -o $name.fasta
+	#zcat $f | fastq_to_fasta | fastx_clipper -a "AGATCGGAAG" -l 18 -c | fastx_trimmer -l 25 | fastx_collapser -o $name.fasta
+	zcat $line --force | fastq_to_fasta | fastx_clipper -a "AGATCGGAAG" -l 18 -c | fastx_trimmer -l 25 | fastx_collapser -o $sample.fasta
 	# sRNAbench
-	java -Xms20m -Xmx14900m -jar $home/exec/sRNAbench.jar input=$name.fasta output=$name.out dbPath=$home graphics=true sep="-" microRNA=hsa species=hg38 mature=mature.fa hairpin=hairpin.fa
-done
+	java -Xms20m -Xmx10000m -jar $home/exec/sRNAbench.jar input=$sample.fasta output=$sample.out predict=true isoMiR=true dbPath=$home graphics=true sep="-" microRNA=hsa species=hg38 mature=mature.fa hairpin=hairpin.fa libs=hg38_prim_ncRNA libs=hg38_prim_RNAcentral libs=hg38_prim_cdna 
+	# Cleanup
+	rm -rf $sample.out/genomeDistribution &
+	rm -rf $sample.out/hairpin
+	rm -rf $sample.out/genome.parsed
+	rm -rf $sample.out/genome.parsed.zip
+done < "$1"
