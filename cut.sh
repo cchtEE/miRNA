2a935f0a6ee7b8d669ce4e584f59cb153498de31
Priit Paluoja <priitpaluoja@gmail.com>
2018-06-11 08:58:01 +0300

    Create cut.sh

---
+++ cut.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Copy important files.
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    location=$line
+    sample=${location##*/}
+    mkdir $sample.results
+    cp $(find $sample -name "results.txt" -o -name "novel.txt" -o -name "novel_mature.fa" -o -name "novel_hairpin.fa" -o -name  "microRNAannotation.txt" -o -name "mature.iso" -o -name "mappingStat.txt" -o -name "isomiR_summary.txt") $sample.results
+done < "$locations"
