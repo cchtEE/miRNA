cbe63cfcaf67067a01e0749679df38e9fa8fbe1b
Priit Paluoja <priitpaluoja@gmail.com>
2018-06-12 15:48:47 +0300

    Update cut.sh

--- cut.sh
+++ cut.sh
@@ -5,5 +5,5 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
     location=$line
     sample=${location##*/}
     mkdir $sample.results
-    cp $(find $sample -name "results.txt" -o -name "novel.txt" -o -name "novel_mature.fa" -o -name "novel_hairpin.fa" -o -name  "microRNAannotation.txt" -o -name "mature.iso" -o -name "mappingStat.txt" -o -name "isomiR_summary.txt") $sample.results
+    cp $(find $sample -name "results.txt" -o -name "novel.txt" -o -name "novel_mature.fa" -o -name "novel_hairpin.fa" -o -name  "microRNAannotation.txt" -o -name "mature.iso" -o -name "mappingStat.txt" -o -name "isomiR_summary.txt" -o -name "mature_sense.grouped") $sample.results
 done < "$locations"
