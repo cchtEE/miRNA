ddd5c32e961eaec49b9a6a383530a9c628a2247c
Priit Paluoja <priitpaluoja@gmail.com>
2018-06-25 14:20:27 +0300

    Delete cut.sh

--- cut.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Copy important files.
-while IFS='' read -r line || [[ -n "$line" ]]; do
-    location=$line
-    sample=${location##*/}
-    mkdir $sample.results
-    cp $(find $sample -name "results.txt" -o -name "novel.txt" -o -name "novel_mature.fa" -o -name "novel_hairpin.fa" -o -name  "microRNAannotation.txt" -o -name "mature.iso" -o -name "mappingStat.txt" -o -name "isomiR_summary.txt" -o -name "mature_sense.grouped") $sample.results
-done < "$locations"
