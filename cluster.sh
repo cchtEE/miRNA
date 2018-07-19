562f11a3050940b97d632b570260474f28780069
Priit Paluoja <priitpaluoja@gmail.com>
2018-07-19 16:02:45 +0300

    Update cluster.sh

--- cluster.sh
+++ cluster.sh
@@ -4,7 +4,7 @@ module load java-1.8.0_40
 module load viennarna-2.8.1
 module load python-3.6.3
 
-source activate rna # edgeR, DEseq, NOISeq
+source activate rna # edgeR, DEseq, NOISeq, Viennarna
 
 home=/gpfs/hpchome/ppaluoja/software/sRNAtoolboxDB
 export PATH="${PATH}:$home/exec/bowtie-1.2.2-linux-x86_64"
